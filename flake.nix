6cc61d4b318ac22f25fa050f26567c126f2c43eb
maddiethecafebabe <maddie@cafebabe.date>
2022-09-01 02:38:12 +0200

    Turn into flake, add dwm as flake input

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-22.05";
+
+    dwm = {
+      url = "github:maddiethecafebabe/dwm";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    nixosConfigurations = {
+      localhost = nixpkgs.lib.nixosSystem {
+        inherit system;
+
+        modules = [
+          ./hosts/localhost/configuration.nix
+          ./modules
+        ];
+
+        specialArgs = {
+          inherit inputs;
+
+          dwm = inputs.dwm.packages.${system}.dwm;
+        };
+      };
+    };
+  };
+}
--- modules/hardware/graphics.nix
+++ modules/hardware/graphics.nix
@@ -1,16 +1,18 @@
- { lib, pkgs, config, ... }:
-  with lib; let
-    cfg = config.modules.hardware.graphics;
-   in {
-   hardware.opengl = {
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}: {
+  hardware.opengl = {
     enable = true;
     driSupport = true;
     driSupport32Bit = true;
     extraPackages = with pkgs; [
-        mesa.drivers
-        rocm-opencl-icd
-        rocm-opencl-runtime
-        clinfo
-      ];
-   };
- }
+      mesa.drivers
+      rocm-opencl-icd
+      rocm-opencl-runtime
+      clinfo
+    ];
+  };
+}
